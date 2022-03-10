6e78adc05f87d29b58f165a610268b789a466046
gejix <geraldjuwah@gmail.com>
2022-03-10 10:33:55 -0800

    Added file 101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+ * main - Write a string to the standard error
+ *
+ * Return: 1 at the end
+ */
+int main(void)
+{
+	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(2, str, 59);
+	return (1);
+}
