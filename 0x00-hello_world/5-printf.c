36d318b199aa89aeeb633bfe3f77112bf097bcee
gejix <geraldjuwah@gmail.com>
2022-03-10 10:03:45 -0800

    Added file 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - prints a string
+ *
+ * Return: 0 always
+ *
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
