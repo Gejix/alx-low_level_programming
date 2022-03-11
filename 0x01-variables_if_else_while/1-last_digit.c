97cc3e22a436315953c9a8449aa13f2eab426f33
gejix <geraldjuwah@gmail.com>
2022-03-11 08:32:41 -0800

    Updated file 1-last_digit.c apeasing betty

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -30,7 +30,7 @@ int main(void)
 		}
 		else
 		{
-			printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastDigt);
+			printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastDigit);
 		}
 		return (0);
 }
