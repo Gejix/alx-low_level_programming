9bdc42729f078ae996bde7c5649c781d51144b23
gejix <geraldjuwah@gmail.com>
2022-03-11 15:04:13 -0800

    Updated file 102-print_comb5.c fixed 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,42 +6,42 @@
  */
 int main(void)
 {
-	int num1, num2, num3, num4, numOut1, numOut2;
-	
-	num1 = num2 = num3 = num4 = 48;
-	while (num4 < 58)
+int num1, num2, num3, num4, numOut1, numOut2;
+
+num1 = num2 = num3 = num4 = 48;
+while (num4 < 58)
+{
+	num3 = 48;
+	while (num3 < 58)
 	{
-		num3 = 48;
-		while (num3 < 58)
+		num2 = 48;
+		while (num2 < 58)
 		{
-			num2 = 48;
-			while (num2 < 58)
+			num1 = 48;
+			while (num1 < 58)
 			{
-				num1 = 48;
-				while (num1 < 58)
+				numOut1 = (num4 * 10) + num3;
+				numOut2 = (num2 * 10) + num1;
+				if (numOut1 < numOut2)
 				{
-					numOut1 = (num4 * 10) + num3;
-					numOut2 = (num2 * 10) + num1;
-					if (numOut1 < numOut2)
-					{
-						putchar(num4);
-						putchar(num3);
-						putchar(' ');
-						putchar(num2);
-						putchar(num1);
-						if (num4 == 57 && num3 == 56 && num2 == 57 && num1 == 57)
-							break;	
-						putchar(',');
-						putchar(' ');
-					}
-					num1++;
+					putchar(num4);
+					putchar(num3);
+					putchar(' ');
+					putchar(num2);
+					putchar(num1);
+					if (num4 == 57 && num3 == 56 && num2 == 57 && num1 == 57)
+						break;	
+					putchar(',');
+					putchar(' ');
 				}
-				num2++;
+				num1++;
 			}
-			num3++;
+			num2++;
 		}
-		num4++;
+		num3++;
 	}
-	putchar('\n');
-	return (0);
+	num4++;
+}
+putchar('\n');
+return (0);
 }
