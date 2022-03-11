4597e3bab244ab4b37b6d6941f65857ee79062c1
gejix <geraldjuwah@gmail.com>
2022-03-11 14:21:44 -0800

    Updated file 102-print_comb5.c fixed

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,26 +6,41 @@
  */
 int main(void)
 {
-	int num1, num2;
-
-	for (num1 = 0; num1 < 100; num1++)
+	int num1, num2, num3, num4, numOut1, numOut2;
+	
+	num1 = num2 = num3 = num4 = 48;
+	while (num4 < 58)
 	{
-		for (num2 = 0; num2 < 100; num2++)
+		num3 = 48;
+		while (num3 < 58)
 		{
-			if (num1 < num2)
+			num2 = 48;
+			while (num2 < 58)
 			{
-				putchar((num1 / 10) + 48);
-				putchar((num1 % 10) + 48);
-				putchar(' ');
-				putchar((num2 / 10) + 48);
-				putchar((num2 / 10) + 48);
-				if (num1 != 98 || num2 != 99)
+				num1 = 48;
+				while (num1 < 58)
 				{
-					putchar(',');
-					putchar(' ');
+					numOut1 = (num4 * 10) + num3;
+					numOut2 = (num2 * 10) + num1;
+					if (numOut1 < numOut2)
+					{
+						putchar(num4);
+						putchar(num3);
+						putchar(' ');
+						putchar(num2);
+						putchar(num1);
+						if (num4 == 57 && num3 == 56 && num2 == 57 && num1 == 57)
+							break;
+						putchar(',');
+						putchar(' ');
+					}
+					num1++
 				}
+				num2++
 			}
+			num3++
 		}
+		num4++
 	}
 	putchar('\n');
 	return (0);
