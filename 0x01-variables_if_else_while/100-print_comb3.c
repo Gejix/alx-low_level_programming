6089be89c653bea8af05ea30c8fdf7e23b8629d6
gejix <geraldjuwah@gmail.com>
2022-03-11 12:45:38 -0800

    Added file 100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Program prints the numbers 00 to 99.
+ *
+ * Return: alway 0
+ */
+int main(void)
+{
+	int num1, num2;
+
+	for (num1 = 0; num1 <= 9; num1++)
+	{
+		for (num2 = 0; num2; num2++)
+		{
+			putchar(num1 + '0');
+			putchar(num2 + '0');
+			if (num1 != 9 || num2 != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n')
+	return (0);
+}
