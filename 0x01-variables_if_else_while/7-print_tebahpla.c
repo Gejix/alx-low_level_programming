47b8d0dc4796018a81ac81424a74b3201e45d8fe
gejix <geraldjuwah@gmail.com>
2022-03-11 11:25:56 -0800

    Added file  7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - print alphabets in lowercase in reverse
+ *
+ * Return: always 0
+ */
+int nain(void)
+{
+	char alpha;
+
+	for (alpha ='z''; alpha >= 'a'; alpha--)
+	{
+		putchar(alpha);
+	}
+	putchar('\n');
+	return (0);
+}
