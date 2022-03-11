779bc76503d2bc2905414fb8e670322c4bdc83d1
gejix <geraldjuwah@gmail.com>
2022-03-11 13:32:58 -0800

    Updated file 101-print_comb4.c betty fix

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -11,22 +11,22 @@ int main(void)
 	for (num1 = 0; num1 < 10; num1++)
 	{
 		for (num2 = 1; num2 < 10; num2++)
+		{
+			for (num3 = 2; num3 < 10; num3++)
 			{
-				for (num3 = 2; num3 < 10; num3++)
+				if (num3 > num2 && num2 > num1)
 				{
-					if (num3 > num2 && num2 > num1)
+					putchar(num1 + '0');
+					putchar(num2 + '0');
+					putchar(num3 + '0');
+					if (num1 != 7 || num2 != 8)
 					{
-						putchar(num1 + '0');
-						putchar(num2 + '0');
-						putchar(num3 + '0');
-						if (num1 != 7 || num2 != 8)
-						{
-							putchar(',');
-							putchar(' ');
-						}
+						putchar(',');
+						putchar(' ');
 					}
 				}
 			}
+		}
 	}
 	putchar('\n');
 	return (0);
