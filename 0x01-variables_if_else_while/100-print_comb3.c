00251273d1db2e663d051e17e1c20e8485c619f6
gejix <geraldjuwah@gmail.com>
2022-03-11 13:10:34 -0800

    Updated file 100-print_comb3.c 1

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,19 +8,16 @@ int main(void)
 {
 	int num1, num2;
 
-	for (num1 = 0; num1 < 10; num1++)
+	for (num1 = 0; num1 <= 9; num1++)
 	{
-		for (num2 = 0; num2 < 10; num2++)
+		for (num2 = num1 + 1; num2 <= 9; num2++)
 		{
-			if (num2 != num1 && num2 < num1)
+			putchar(num1 + '0');
+			putchar(num2 + '0');
+			if (num1 < 8)
 			{
-				putchar(num1 + '0');
-				putchar(num2 + '0');
-				if (num1 != 9 && num2 != 9)
-				{
-					putchar(',');
-					putchar(' ');
-				}
+				putchar(',');
+				putchar(' ');
 			}
 		}
 	}
