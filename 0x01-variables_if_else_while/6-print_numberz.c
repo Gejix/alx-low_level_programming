dbfce3f5d00f9883fad3f860f3511e7ad02d49a1
gejix <geraldjuwah@gmail.com>
2022-03-11 11:08:24 -0800

    Updatewqd file 6-print_numberz.c betty fix

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - A program that prints all single digit numbers of base 10 starting from 0.
+ * main - Prints all single digit numbers of base 10 starting from 0.
  * followed by a new line
  * Return: alway 0
  */
