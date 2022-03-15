58d51ac9c780b39e0399510eaafe52670ae56fc7
gejix <geraldjuwah@gmail.com>
2022-03-15 08:41:51 -0700

    Updated file 0-putchar.c on multiple lines

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,7 +5,14 @@
  */
 int main(void)
 {
-	_putchar("_putchar");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 
 	return (0);
