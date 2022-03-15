00856a9ff9e991eb7977aef69d81f2ec33aa3d4b
gejix <geraldjuwah@gmail.com>
2022-03-15 16:46:05 -0700

    Added file 102-fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Program that prints the add of the fibonacci numbers
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int c;
+	long int x, y, z;
+
+	x = 1;
+	y = 2;
+	printf("%ld, %ld", x, y);
+	for (c = 0; c < 48; c++)
+	{
+		z = x + y;
+		printf(", %ld" z);
+		x = y;
+		y = z;
+	}
+	printf("\n";
+	return (0);
+}
+	
