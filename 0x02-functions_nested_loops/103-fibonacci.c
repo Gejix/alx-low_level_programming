3ec22e4205ddd30229661d1a8425fd5799de53d6
gejix <geraldjuwah@gmail.com>
2022-03-15 16:58:49 -0700

    added file  103-fibonacci.c

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main -Program that prints the add of the even-valued
+ * terms
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	long int x, y, z, outp;
+
+	x = 1;
+	y = 2;
+	z = outp = 0;
+	whaile (z <= 4000000)
+	{
+		z = x + y;
+		x = y;
+		y = z;
+		if ((x % 2) == 0)
+		{
+			outp += x;
+		}
+	}
+	printf("%ld\n", outp);
+	return (0);
+}
