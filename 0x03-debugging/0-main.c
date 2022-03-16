e35ac731bb9abdd4f904112ee6d28bfddd3fc09b
gejix <geraldjuwah@gmail.com>
2022-03-15 18:12:22 -0700

    Updated file  0-main.c 1

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main - Prints if tts function that prints integer is positive or negative
+ * main - tests funtion that prints if integer is positive or negative
  * Return: 0
  */
 
