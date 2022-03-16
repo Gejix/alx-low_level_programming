3630a8b725989c648a8b56a33cfeb96295563635
gejix <geraldjuwah@gmail.com>
2022-03-16 04:05:14 -0700

    Updated file betty fixed 8

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -27,9 +27,8 @@ void print_remaining_days(int month, int day, int year)
 	{
 		if (month == 2 && day == 60)
 		{
-	printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
+			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 		}
-		
 		else
 		{
 			printf("Day of the year: %d\n", day);
