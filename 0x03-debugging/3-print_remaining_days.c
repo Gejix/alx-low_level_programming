f1cc8e8a692bd3ffd3e0ba233e358c2151cdf332
gejix <geraldjuwah@gmail.com>
2022-03-16 02:28:44 -0700

    Updated file  3-print_remaining_days.c betty bug fix 1

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -16,8 +16,9 @@ void print_remaining_days(int month, int day, int year)
 	{
 		if (month > 2 && day >= 60)
 		{
-			day++;
+		day++;
 		}
+
 		printf("Day of the year: %d\n", day);
 		printf("Remaining days: %d\n", 366 - day);
 	}
