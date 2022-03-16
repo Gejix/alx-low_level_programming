2c520794bb89e127fb8968cbcc7231f4918f490d
gejix <geraldjuwah@gmail.com>
2022-03-15 17:15:37 -0700

    Added file 104-fibonacci.c

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints the first 98 Fibonacci numbers
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int k, boolean, boolean1;
+	long int num1, num2, fn, fn1, num11, num22;
+	
+	num1 = 1;
+	num2 = 2;
+	boolean = boolean1 = 1;
+	printf("%ld, %ld", num1, num2);
+	for (k = 0; k < 96; k++)
+	{
+		if (boolean)
+		{
+			fn = num1 + num2;
+			printf(", %ld", fn);
+			num1 = num2;
+			num2 = fn;
+		}
+		else
+		{
+			if (boolean2)
+			{
+				num11 = num1 % 1000000000;
+				num22 = num2 % 1000000000;
+				num1 = num1 / 1000000000;
+				num2 = num2 / 1000000000;
+				boolean2 = 0;
+			}
+			fn1 = (num11 + num22);
+			fn = num1 + num2 + ( fn1 / 1000000000);
+			printf(", %ld", fn);
+			printf("%ld", fn1 % 1000000000);
+			num1 = num2;
+			num11 = num22;
+			num2 = fn;
+			num22 = fn1 % 1000000000);
+		}
+		if (((num1 + num2) < 0) && boolean == 1)
+			boolean = 0;
+	}
+	print("\n");
+	return (0);
+}
