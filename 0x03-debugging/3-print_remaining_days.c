c74ca18f218b06d23518fde8d567345deb1f2eb5
gejix <geraldjuwah@gmail.com>
2022-03-16 04:10:47 -0700

    Updated file betty fixed 9

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -22,7 +22,6 @@ void print_remaining_days(int month, int day, int year)
 		printf("Day of the year: %d\n", day);
 		printf("Remaining days: %d\n", 366 - day);
 	}
-
 	else
 	{
 		if (month == 2 && day == 60)
