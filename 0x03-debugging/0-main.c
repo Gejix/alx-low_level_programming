fdd11582d651450bc67fcb2e41a913efb8f4ae84
gejix <geraldjuwah@gmail.com>
2022-03-15 18:03:36 -0700

    Added file  0-main.c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - Prints if the integer is positive or negative
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	i = 98;
+	positive_or_negative(i);
+
+	return (0);
+}
