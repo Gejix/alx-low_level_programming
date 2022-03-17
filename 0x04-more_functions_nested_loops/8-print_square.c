9c8f779ee0555779c42ecb1862b81d05347a7349
gejix <geraldjuwah@gmail.com>
2022-03-17 11:27:48 -0700

    Added file 8-print_square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_square - prints hashes 
+ * @size: size of the square
+ * Return: no return
+ */
+void print_square(int size)
+{
+	int i, j;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			_putchar('#');
+		}
+		if (i != size - 1)
+			_putchar('\n');
+	}
+	_putchar('\n');
+}
