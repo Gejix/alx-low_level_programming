604007bb9c1cc0f99a60a108648d639ec204f899
gejix <geraldjuwah@gmail.com>
2022-03-17 10:15:53 -0700

    Updated file 4-print_most_numbers.c 4 betty fix

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print-most_numbers - prints the numbers from 0 to 9
+ * print_most_numbers - prints the numbers from 0 to 9
  * except 2 and 4
  * Return: no return
  */
