32ea4a105acebce8f33af692688c643454311d47
gejix <geraldjuwah@gmail.com>
2022-03-17 09:41:24 -0700

    Added file 3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers between 0 to 9.
+ * Return: no return.
+ */
+void print_number(void)
+{
+	int num;
+
+	for (num = '0'; num < '9'; num++)
+	{
+		_putchar(num);
+	}
+
+	_putchar('\n');
+}
