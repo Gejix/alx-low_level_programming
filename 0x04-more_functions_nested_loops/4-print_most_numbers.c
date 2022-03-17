cb11a7ffb643edd1a15f33d318d7b9dbc4215d5d
gejix <geraldjuwah@gmail.com>
2022-03-17 10:14:28 -0700

    Updated file 4-print_most_numbers.c 3

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,7 +2,8 @@
 
 /**
  * print-most_numbers - prints the numbers from 0 to 9
- * except 2 and 4, followed by a new line
+ * except 2 and 4
+ * Return: no return
  */
 void print_most_numbers(void)
 {
