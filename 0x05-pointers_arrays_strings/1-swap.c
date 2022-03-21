01ec1f4f6cdaea28412ab808626b6eb7d2532d8f
gejix <geraldjuwah@gmail.com>
2022-03-21 11:48:53 -0700

    Added file 1-swap.c betty fix

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,11 +1,13 @@
 #include "main.h"
 
 /**
- * swap_int - is a function that swaps the value 
+ * swap_int - is a function that swaps the value
  * of two integers.
- *
- * Return: Nothing
+ * @a: first integer
+ * @b: second integer
+ * Return: no return
  */
+
 void swap_int(int *a, int *b)
 {
 	int c;
