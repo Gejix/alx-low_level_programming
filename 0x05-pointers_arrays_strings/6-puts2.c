f3be3a2cf9bc4fbb1c5ad2a8fc77e36d7cb6fa3e
gejix <geraldjuwah@gmail.com>
2022-03-21 13:07:49 -0700

    Added file 6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * puts2 - A function that prints one char out of 2 of a string.
+ * @str: string
+ * Return: no return
+ */
+void puts(char *str)
+{
+	int count = 0;
+
+	while (count >= 0)
+	{
+		if (str[count] == '\0')
+		{
+			_putchar('\n')
+			break;
+		}
+		if (count % 2 == 0)
+			_putchar(str[count]);
+		count++;
+	}
+}
