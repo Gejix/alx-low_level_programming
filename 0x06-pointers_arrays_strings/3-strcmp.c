3731a10036c16330ae90d5c568a76929f5291696
gejix <geraldjuwah@gmail.com>
2022-03-23 16:29:45 -0700

    Updated file 3-strcmp.c betty fix

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -5,7 +5,7 @@
  * @s1: first string to compare
  * @s2: second string to compare
  *
- * Retun: less than 0 if s1 is less than s2, 0 if they are
+ * Return: less than 0 if s1 is less than s2, 0 if they are
  * equal, more than 0 if s1 is greater than s2
  */
 int _strcmp(char *s1, char *s2)
