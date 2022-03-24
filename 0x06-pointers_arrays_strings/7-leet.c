d0a38dc8816a46009680f79ce32e16ffa661c29c
gejix <geraldjuwah@gmail.com>
2022-03-23 17:26:08 -0700

    Updated file 7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -6,7 +6,7 @@
  */
 char *leet(char *s)
 {
-	int count = o, i;
+	int count = 0, i;
 	int low_letters[] = {97, 101, 111, 116, 108};
 	int upp_letters[] = {65, 69, 79, 84, 76};
 	int numbers[] = {52, 51, 48, 55, 49};
