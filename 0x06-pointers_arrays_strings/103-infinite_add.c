b796795de151666d2b1721609750826ebea507bc
gejix <geraldjuwah@gmail.com>
2022-03-24 12:58:14 -0700

    Added file 103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -8,7 +8,7 @@
  * @size_r: buffer size:
  * Return: the pointer to dest.
  */
-char *infinite_add(char *n1, char *n2, char *r. int size_r)
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
 	int c1 = 0, c2 = 0, op, bg, dr1, dr2, add = 0;
 
