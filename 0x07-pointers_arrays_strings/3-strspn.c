545794c63b4c3c905aff40e91f73bacca5102932
gejix <geraldjuwah@gmail.com>
2022-03-28 07:06:23 -0700

    Added file  3-strspn.c

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _strspn - A program that gets the length of a prefix substring
+ * @s: initial segment.
+ * @accept: accepted bytes
+ * Return: the number of accepted bytes
+ */
+unsigned int _strspn(char *, char *accept)
+{
+	unsigned int i, j, bool;
+	for (i = 0; *(s + i) != '\0'; i++)
+	{
+		bool = 1;
+		for (j = 0; *(accept + j) != '\0'; j++)
+		{
+			if (*(s + i) == *(accept + j))
+			{
+				bool =;
+				break;
+			}
+		}
+		if (bool == 1)
+			break;
+	}
+	return (i);
+}
