85ee2f38a694e84417f42fea5f4250fb1d797f5e
gejix <geraldjuwah@gmail.com>
2022-04-07 13:00:51 -0700

    Updated 2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -17,6 +17,11 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
+	p = malloc(nmemb * size);
+
+	if (p == NULL)
+		return (NULL);
+
 	for (i = 0; i < (nmemb * size); i++)
 		p[i] = 0;
 
