2eb81aea59cad3177354e9ca31073410fa0e1810
gejix <geraldjuwah@gmail.com>
2022-04-11 14:57:35 -0700

    Updated file 3-function_like_macro.h

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,6 @@
-#ifndef _FUNCTION_LIKE_MACRO_
-#define _FUNCTION_LIKE_MACR0_
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
 
-#define ABS(x) ((x) < (0) ? ((x) * (-1)) : (x))
+#define ABS(x) ((x) < 0 ? (-x) : (x))
 
 #endif
