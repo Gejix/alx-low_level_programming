ac0a4f71d431f0426384fdb89326cbfd9e4ef9a5
gejix <geraldjuwah@gmail.com>
2022-04-13 15:04:33 -0700

    Added file 3-main.c

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,40 @@
+#unclude "3-calc.h"
+
+/**
+ * main - check the code for ALX School students.
+ * @argc: argument count.
+ * @argv: argument vector.
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	int a, b;
+	int (*operation)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	if (arg[2][1])
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	operation = get_op_fuc(argv[2]);
+
+	if (operation == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+
+	printf("%d\n", operation(a, b));
+	return (0);
+}
