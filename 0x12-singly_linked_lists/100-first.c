1885f40e62367c4b0b554c2099e14a439847c643
gejix <geraldjuwah@gmail.com>
2022-04-21 14:05:38 -0700

    [feat]: Added new file 100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+ * first - Prints a sentence before the main.c
+ * function is executed
+ */
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
