f1471ebaf62990ff96dc9268ef2e25169a5cdf04
gejix <geraldjuwah@gmail.com>
2022-04-21 15:52:14 -0700

    [fix]: bug fix

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -2,16 +2,15 @@
 	extern printf
 
 	section .data
-msg: 	db "Hello, Holberton",0
+msg: 	db "Hello, Holberton", 0
 format: db "%s", 10, 0
 
 	section .text
 main:
 	mov edi, msg
 	mov edi, format
-	xor eax, eax
+	mov eax, 0
 	call printf
 
 	mov eax, 0
-	leave
 	ret
