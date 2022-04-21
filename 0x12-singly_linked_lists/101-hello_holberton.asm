1180ead52789ab18aac063f8c90c48113ed4bbfd
gejix <geraldjuwah@gmail.com>
2022-04-21 15:46:08 -0700

    [fix]: Update file, bug fix 5

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -6,14 +6,12 @@ msg: 	db "Hello, Holberton",0
 format: db "%s", 10, 0
 
 	section .text
-main:   push rbp
-        mov rbp, rsp
-	sub rsp, 16
-
+main:
+	mov edi, msg
+	mov edi, format
 	xor eax, eax
-	lea rdi, [msg]
 	call printf
 	
-	add rsp, 16
+	mov eax, 0
 	leave
 	ret
