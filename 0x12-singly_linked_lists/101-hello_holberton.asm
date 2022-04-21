931a01632bd5188343de42c2f4b9a0f54a67f449
gejix <geraldjuwah@gmail.com>
2022-04-21 15:33:34 -0700

    [fix]: Update file, bug fix 3

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -2,17 +2,21 @@ global main
 extern printf
 
 section .data
-msg db "Hello, Holberton", 0
+	msg: db "Hello, Holberton",0
+	format: db "%s", 10, 0
+
+section .text
+main:   push rbp
+        mov rbp, rsp
+	sub rsp, 16
+
+	xor eax, eax
+	lea rdi, [msg]
+	call printf
+	
+	add rsp, 16
+	leave
+	ret
 
 section .bss
-section .text
-main:   push ebp
-        mov ebp, esp
-
-	push msg
-	call printf
-	add esp, 4
-
-        mov esp, ebp
-	pop ebp
-ret
+	number resb 4
