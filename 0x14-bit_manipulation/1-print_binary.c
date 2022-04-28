b64bf3b3ed1bf797a5ceeb375c31df0182363028
Gejix <geraldjuwah@gmail.com>
2022-04-28 22:25:38 +0100

    [style]: White space fix

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,26 +1,26 @@
 #include "main.h"
 
 /**
- * print_binary - prints the binary of a decimal
+ * print_binary - prints the binary of a decimal number
  * @n: number to print in binary
  */
 void print_binary(unsigned long int n)
 {
-    int i, count = 0;
-    unsigned long int current;
+	int i, count = 0;
+	unsigned long int current;
 
-    for (i = 31; i >= 0; i--)
-    {
-        current = n >> i;
+	for (i = 31; i >= 0; i--)
+	{
+		current = n >> i;
 
-        if (current & 1)
-        {
-            _putchar('1');
-            count++;
-        }
-        else if (count)
-            _putchar('0');
-    }
-    if (!count)
-        _putchar('0');
+		if (current & 1)
+		{
+			_putchar('1');
+			count++;
+		}
+		else if (count)
+			_putchar('0');
+	}
+	if (!count)
+		_putchar('0');
 }
