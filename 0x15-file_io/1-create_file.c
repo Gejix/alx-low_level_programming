fdee93f91780ae12272432b6e6d5c66fbb98e90e
Gejix <geraldjuwah@gmail.com>
2022-05-02 22:43:40 +0100

    [style]: Betty bug fix

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,30 +9,30 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-    int fd;
-    int nchars;
-    int wrt;
+	int fd;
+	int nchars;
+	int wrt;
 
-    if (!filename)
-        return (-1);
+	if (!filename)
+		return (-1);
 
-    fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
-    if (fd == -1)
-        return (-1);
+	if (fd == -1)
+		return (-1);
 
-    if (!text_content)
-        text_content = "";
+	if (!text_content)
+		text_content = "";
 
-    for (nchars = 0; text_content[nchars]; nchars++)
-        ;
+	for (nchars = 0; text_content[nchars]; nchars++)
+		;
 
-    wrt = write(fd, text_content, nchars);
+	wrt = write(fd, text_content, nchars);
 
-    if (wrt == -1)
-        return (-1);
+	if (wrt == -1)
+		return (-1);
 
-    close(fd);
+	close(fd);
 
-    return (1);
+	return (1);
 }
